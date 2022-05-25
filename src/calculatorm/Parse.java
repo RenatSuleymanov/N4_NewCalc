22b321e973d56a637693ba0a01894e2d81f5f1d0
RenatSuleymanov <Renat.Suleymanov.job@users.noreply.github.com>
2022-05-26 02:34:46 +0500

    Initial commit

---
+++ src/calculatorm/Parse.java
@@ -0,0 +1,18 @@
+package calculatorm;
+
+public class Parse {
+    public boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    //---проверка корректности оператора, должно быть: +-*/
+    public boolean checkOperator(String op){
+        return "*".equals(op) || "/".equals(op) || "+".equals(op) || "-".equals(op);
+    }
+
+}
